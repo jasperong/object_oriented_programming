657c06545fa2684411a85a66ae5ffafd1d6fec1c
Jasper <jave.ong4@yahoo.com>
2016-06-01 14:52:06 -0400

    Added person parent class

--- people.rb
+++ people.rb
@@ -1,10 +1,22 @@
-class Student
+class Person
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+  def greeting
+    "Hi, my name is #{@name}"
+  end
+end
+
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object."
   end
