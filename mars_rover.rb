57d8deca338a223a877c26a6eb908cad8965bce1
Jasper <jave.ong4@yahoo.com>
2016-06-01 16:52:13 -0400

    Added move

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,19 +1,34 @@
 class Rover
-  attr_accessor :x, :y, :direction, :move, :turn
-  
-  def initialize
+  attr_accessor :x, :y, :direction, :steps, :turn
+
+  def initialize(x, y, direction)
+
 
   end
 
-  def read_instruction
+  def read_instruction(direction)
 
   end
 
-  def move
-
+  def move(steps)
+    case @direction
+    when n
+      y += 1
+    when e
+      x += 1
+    when s
+      y -= 1
+    when w
+      x -= 1
+    end
   end
 
-  def turn
+  def turn(turn)
+    puts "Enter L to turn left"
+    puts "Enter R to turn right"
 
   end
 end
+
+rover1 = Rover.new
+rover1.read_instruction
