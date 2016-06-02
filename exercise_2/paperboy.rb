c56c60beaebcfeedf3cf6c5cf0ce0a145ec63374
Jasper <jave.ong4@yahoo.com>
2016-06-02 16:33:37 -0400

    Math is still wrong

--- exercise_2/paperboy.rb
+++ exercise_2/paperboy.rb
@@ -4,24 +4,40 @@ class Paperboy
   def initialize(name, side)
     @name = name
     @side = side
-  end
-
-  def deliver(start_address, end_address)
-    @experience = (end_address - start_address) / 2
-    quota = 50
-    if @experience < quota
-      under_quota = @experience * 0.25
-    else
-       over_quota = (@experience * 0.50) - (50 * 0.25)
-    end
+    @quota = 50
+    @experience = 0
+    @earnings = 0
   end
 
   def quota
-    ( @experience / 2 ) + 50
+    if @experience == nil
+      @quota
+    else
+      @experience / 2 + @quota
+    end
   end
+
+  def deliver(start_address, end_address)
+    deliveries = (end_address - start_address) / 2
+    deliveries.times do |delivery|
+      if deliveries < quota
+        @total = deliveries * 0.25
+      elsif deliveries >= quota
+        @total = (deliveries - quota) * 0.50 + quota * 0.25
+      end
+      @experience += 1
+    end
+    @total
+  end
+
 
   def report
     "Im #{name}, I've delivered #{@experience} papers and #{@earnings}"
   end
-
 end
+
+tommy = Paperboy.new("Tommy", "odd")
+
+puts tommy.quota
+puts tommy.deliver(100, 220)
+puts tommy.quota
