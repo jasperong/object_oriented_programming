f3ce51903fb03778229ed510f274b516c32978fb
Jasper <jave.ong4@yahoo.com>
2016-06-02 13:27:14 -0400

    Created meal_time method

--- exercise_1/cat.rb
+++ exercise_1/cat.rb
@@ -1,4 +1,4 @@
-class cat
+class Cat
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
@@ -6,5 +6,16 @@ class cat
   end
 end
 
-kitty = Cat.new("kitty", "tuna", "12 PM")
-neko = Cat.new("neko", "salmon", "3 PM")
+kitty = Cat.new("kitty", "tuna", 11)
+neko = Cat.new("neko", "salmon", 14)
+
+def meal_time
+
+  time = [*1..12]
+  if @meal_time <= 11
+    "#{time[@meal_time]} + AM"
+  else
+    "#{time[@meal_time - 12]} + PM"
+  end
+  meal_time
+end
