5c0d3803739404585db6d5f02f73c3a8c67c1225
Jasper <jave.ong4@yahoo.com>
2016-06-02 09:25:34 -0400

    sales_tax beta

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,47 +1,49 @@
-class Float
-  def round5
-    (self * 10.0).round / 10.0
-  end
-end
-
 def total_price(items)
 
   items.map do |item, price|
     @basic_tax = price * 0.1
     @imp_tax = price * 0.05
     @total_tax = @basic_tax + @imp_tax
+
+    if item.include?("book" || "bar" || "pills")
+      return price
+    elsif item.include?("imported")
+      return price + @total_tax
+    else
+      return price + @basic_tax
+    end
   end
 
   items.each do |item, price|
-    puts "#{item}: #{(price + @total_tax).round5}"
+    puts "#{item}: #{(price + @total_tax).round(2)}"
   end
 
-    puts "Sales Taxes: #{@total_tax.round5}"
+  puts "Sales Taxes: #{@total_tax.round(2)}"
 
   total = items.each_value.inject(:+) + @total_tax
 
-  puts "Total: #{total.round5}"
+  puts "Total: #{total.round(2)}"
 
 end
 
-cart_1 = {
+  cart_1 = {
   "book" => 12.49,
   "music CD" => 14.99,
   "chocolate bar" => 0.85
-}
+  }
 
-cart_2 = {
+  cart_2 = {
   "imported box of chocolates" => 10.00,
   "imported bottle of perfume" => 47.50
-}
+  }
 
-cart_3 = {
+  cart_3 = {
   "imported bottle of perfume" => 27.99,
   "bottle of perfume" => 18.99,
   "packet of headache pills" => 9.75,
   "box of imported chocolates" => 11.25
-}
+  }
 
-total_price(cart_1)
-total_price(cart_2)
-total_price(cart_3)
+  total_price(cart_1)
+  total_price(cart_2)
+  total_price(cart_3)
