0b3c24da652985d6e08e826fd09a6934481661cd
Jasper <jave.ong4@yahoo.com>
2016-06-02 13:47:43 -0400

    Fixed meal_time method

--- exercise_1/cat.rb
+++ exercise_1/cat.rb
@@ -1,21 +1,24 @@
 class Cat
+
+  attr_accessor :name, :preferred_food, :meal_time
+
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
+
+  def meal_time
+    if @meal_time <= 11
+      "#{@meal_time + 1} AM"
+    else
+      "#{@meal_time - 11} PM"
+    end
+  end
 end
+
 
 kitty = Cat.new("kitty", "tuna", 11)
 neko = Cat.new("neko", "salmon", 14)
 
-def meal_time
-
-  time = [*1..12]
-  if @meal_time <= 11
-    "#{time[@meal_time]} + AM"
-  else
-    "#{time[@meal_time - 12]} + PM"
-  end
-  meal_time
-end
+puts kitty.meal_time
