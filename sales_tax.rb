a6fd90032d6ea6406dc15f178ff2ebcc6a82d875
Jasper <jave.ong4@yahoo.com>
2016-06-01 21:22:50 -0400

    sales_tax.rb alpha v2

---
+++ sales_tax.rb
@@ -0,0 +1,29 @@
+def total_price(items)
+
+  items.map do |item, price|
+    basic_tax = price * 0.1
+    imp_tax = price * 0.05
+    @total_tax = basic_tax + imp_tax
+  end
+
+  items.each do |item, price|
+    puts "#{item}: #{price + @total_tax}"
+  end
+
+  items.each_value do
+    puts "Sales Taxes: #{@total_tax}"
+  end
+
+  total = items.each_value.inject(:+)
+
+  puts "Total: #{total}"
+
+end
+
+cart_1 = {
+  "book" => 12.49,
+  "music CD" => 14.99,
+  "chocolate bar" => 0.85
+}
+
+total_price(cart_1)
