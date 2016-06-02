7d998db597e78fbd03c45781fca2b0d37b6ac5d6
Jasper <jave.ong4@yahoo.com>
2016-06-02 19:14:36 -0400

    almost there

--- mars_rover-2_0.rb
+++ mars_rover-2_0.rb
@@ -10,46 +10,43 @@ class Rover
   def read_instruction
     puts "Enter 1st rover initial position and direction"
     @initial = gets.chomp.gsub(" ", "").split("")
-      @initial.each do |x, y, direction|
-      end
+      @x = @initial[0]
+      @y = @initial[1]
+      @direction = @initial[2]
     puts "Enter move instructions"
     @instructions = gets.chomp.gsub(" ", "").split("")
     puts "Enter 2nd rover initial position and direction"
   end
 
-
-
   def turn(turn)
     @instructions.each do |command|
       case command == "L"
-      when @initial.include?("N") then "W"
-      when @initial.include?("W") then "S"
-      when @initial.include?("S") then "E"
-      when @initial.include?("E") then "N"
+      when @initial.include?("N") then @w
+      when @initial.include?("W") then @s
+      when @initial.include?("S") then @e
+      when @initial.include?("E") then @n
       end
       case command == "R"
-      when @initial.include?("N") then "E"
-      when @initial.include?("E") then "S"
-      when @initial.include?("S") then "W"
-      when @initial.include?("W") then "N"
+      when @initial.include?("N") then @e
+      when @initial.include?("E") then @s
+      when @initial.include?("S") then @w
+      when @initial.include?("W") then @n
       end
     end
   end
 
   def move
     @instructions.each do |command|
-      case command
-        when "N" then y += 1
-        when "E" then x += 1
-        when "S" then y -= 1
-        when "W" then x -= 1
+      case command == "M"
+      when @n then @y += 1
+      when @e then @x += 1
+      when @s then @y -= 1
+      when @w then @x -= 1
       end
     end
+    "#{@x} #{@y} #{@direction}"
   end
 end
 
 puts "Enter grid size"
 @size = gets.chomp.gsub(" ", "").split("")
-
-rover1 = Rover.new
-rover1.read_instruction
